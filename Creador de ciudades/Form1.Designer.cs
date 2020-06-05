05fb9a304f9db640b031fcd04b9961d45e36e7d4
MRCHEESE97 <46450049+MRCHEESE97@users.noreply.github.com>
2020-06-04 20:07:29 -0500

    0.003-UI-1
    Se creó la primera UI está vez considerando la cantidad de plantas para las casas

--- Creador de ciudades/Form1.Designer.cs
+++ Creador de ciudades/Form1.Designer.cs
@@ -29,12 +29,143 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.temaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBoxCasas = new System.Windows.Forms.GroupBox();
+            this.groupBoxInteriores = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBoxGeneral = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.temaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(803, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // temaToolStripMenuItem
+            // 
+            this.temaToolStripMenuItem.Name = "temaToolStripMenuItem";
+            this.temaToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.temaToolStripMenuItem.Text = "Tema";
+            // 
+            // groupBoxCasas
+            // 
+            this.groupBoxCasas.Location = new System.Drawing.Point(368, 157);
+            this.groupBoxCasas.Name = "groupBoxCasas";
+            this.groupBoxCasas.Size = new System.Drawing.Size(212, 293);
+            this.groupBoxCasas.TabIndex = 4;
+            this.groupBoxCasas.TabStop = false;
+            this.groupBoxCasas.Text = "Propiedades de las casas";
+            // 
+            // groupBoxInteriores
+            // 
+            this.groupBoxInteriores.Location = new System.Drawing.Point(586, 157);
+            this.groupBoxInteriores.Name = "groupBoxInteriores";
+            this.groupBoxInteriores.Size = new System.Drawing.Size(212, 293);
+            this.groupBoxInteriores.TabIndex = 5;
+            this.groupBoxInteriores.TabStop = false;
+            this.groupBoxInteriores.Text = "Propiedades de los interiores";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(350, 426);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AutoScroll = true;
+            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(342, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "PB";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxGeneral
+            // 
+            this.groupBoxGeneral.Location = new System.Drawing.Point(368, 24);
+            this.groupBoxGeneral.Name = "groupBoxGeneral";
+            this.groupBoxGeneral.Size = new System.Drawing.Size(430, 127);
+            this.groupBoxGeneral.TabIndex = 7;
+            this.groupBoxGeneral.TabStop = false;
+            this.groupBoxGeneral.Text = "Propiedades de las plantas";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(370, 388);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.groupBoxGeneral);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.groupBoxInteriores);
+            this.Controls.Add(this.groupBoxCasas);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem temaToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBoxCasas;
+        private System.Windows.Forms.GroupBox groupBoxInteriores;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBoxGeneral;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
